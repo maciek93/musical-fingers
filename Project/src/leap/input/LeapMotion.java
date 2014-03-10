39e4c6da787b931a75353a4c1095a8ac20c2cd96
Maciej <maciek93@gmail.com>
2014-03-10 02:06:20 +0000

    Added LeapMotion class

---
+++ Project/src/leap/input/LeapMotion.java
@@ -0,0 +1,72 @@
+package leap.input;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Finger;
+import com.leapmotion.leap.FingerList;
+import com.leapmotion.leap.Frame;
+import com.leapmotion.leap.Gesture;
+import com.leapmotion.leap.GestureList;
+import com.leapmotion.leap.KeyTapGesture;
+import com.leapmotion.leap.Pointable;
+
+public class LeapMotion {
+	
+	private PianoListener pianoListener = new PianoListener(this);
+	private Controller controller = new Controller();
+	List<Finger> fingers = new LinkedList<Finger>();
+	Set<Gesture> currentGestures = new HashSet<Gesture>();
+	
+	public LeapMotion() {
+		controller.addListener(pianoListener);
+	}
+	
+	public boolean[] processData(Frame frame) {
+		FingerList newFingers = frame.fingers();
+		for(Iterator<Finger> fingersIter = newFingers.iterator(); fingersIter.hasNext();) {
+			Finger finger = fingersIter.next();
+			if(!fingers.contains(finger)) {
+				identifyFingers(newFingers);
+				break;
+			}
+		}
+		GestureList newGestures = frame.gestures();
+		boolean[] taps = new boolean[10];
+		Set<Gesture> update = new HashSet<Gesture>();
+		for(Iterator<Gesture> gestureIter = newGestures.iterator(); gestureIter.hasNext();) {
+			Gesture gesture = gestureIter.next();
+			if(!currentGestures.contains(gesture)) {
+				taps[processGesture((KeyTapGesture) gesture)] = true;
+			}
+			update.add(gesture);
+		}
+		currentGestures = update;
+		return taps;
+	}
+
+	private int processGesture(KeyTapGesture gesture) {
+		Pointable finger = gesture.pointable();
+		return fingers.indexOf(finger);
+	}
+
+	private void identifyFingers(FingerList newFingers) {
+		HashMap<Float, Finger> fingerMap = new HashMap<Float, Finger>();
+		List<Finger> update = new LinkedList<Finger>();
+		for(Iterator<Finger> fingers = newFingers.iterator(); fingers.hasNext();) {
+			Finger finger = fingers.next();
+			fingerMap.put(finger.tipPosition().getX(), finger);
+		}
+		Float[] positions = (Float[]) fingerMap.keySet().toArray();
+		Arrays.sort(positions);
+		for(int i = 0; i < 10; i++) {
+			update.add(fingerMap.get(positions[i]));
+		}
+		fingers = update;
+	}
+}
---
+++ Project/src/leap/input/PianoListener.java
@@ -0,0 +1,30 @@
+package leap.input;
+
+import java.io.IOException;
+import java.lang.Math;
+
+import com.leapmotion.leap.*;
+import com.leapmotion.leap.Gesture.State;
+
+
+/** Has a list of gestures (only finger taps) and a list of fingers.
+ * The gestures and fingers are public so that they can be accessed by the main program
+ * But we may want to create accessors for this instead.  */
+class PianoListener extends Listener {
+	
+    private LeapMotion leap;
+
+    public PianoListener(LeapMotion leap) {
+    	this.leap = leap;
+    }
+
+    public void onConnect(Controller controller) {
+        controller.enableGesture(Gesture.Type.TYPE_KEY_TAP);
+    }
+
+    public void onFrame(Controller controller) {
+        Frame frame = controller.frame();
+        leap.processData(frame);
+    }
+}
+
