ba6088ec146a81f8dd6982ec78f14193a0db51e0
blutooth <maximilian@live.co.uk>
2014-05-13 18:07:26 +0100

    alternative drum
    palm tap drum

--- Musical-fingers/src/ox/musicalfingers/instrument/drum/Drum.java
+++ Musical-fingers/src/ox/musicalfingers/instrument/drum/Drum.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.leapmotion.leap.Controller;
 import com.leapmotion.leap.Pointable;
+import com.leapmotion.leap.*;
 import com.leapmotion.leap.PointableList;
 import com.leapmotion.leap.InteractionBox;
 import com.leapmotion.leap.Listener;
@@ -23,8 +24,12 @@ public class Drum extends Listener implements DiscreteInputDisplay{
 	Texture fingerPoint;
 	Texture rectangle;
 	
+	float scaleFactor=1f;
 	int sF = 1;
+	int timeout=10;
+	float Radius=0.5f;
 	
+	HandList hands;
 	PointableList pointableList;
 	InteractionBox iBox;
 	
@@ -40,6 +45,7 @@ public class Drum extends Listener implements DiscreteInputDisplay{
 		sF =  MusicalFingers.width/drumImg.getWidth();
 		sF -= 2;
 		
+		
 		//Rectangle
 		Pixmap pixmap = new Pixmap( 1,1, Format.RGBA8888 );
 		pixmap.setColor( 1,1,1,1);
@@ -50,38 +56,42 @@ public class Drum extends Listener implements DiscreteInputDisplay{
     public void onInit (Controller controller) {
         System.out.println("Initialized Piano");
         pointableList = controller.frame().pointables();
+        hands = controller.frame().hands();
+    	iBox = controller.frame().interactionBox();
     }
     
     public void onFrame(Controller controller) {
     	iBox = controller.frame().interactionBox();
-    	
+    	timeout-=1;
 		for (int i = 0; i < 5; i++) {
 			notes[i] = false; // Stop starting previous sound.
 		}
     	
     	if (iBox.isValid()) {
+    	    hands =controller.frame().hands();
 	    	pointableList = controller.frame().pointables();
-	    	if (pointableList.count() > 0) {
-	    		for (Pointable pointable : pointableList) {
-	    		// Pointable pointable = lowestPointable(pointableList); // gets the pointable with the lowest y value.
-		    		Vector pointablePos = vectorMinus ( iBox.normalizePoint(pointable.tipPosition(),false) , midPoint ) ;
-		    		if (pointablePos.getY() < 0 && isAbove) {
+	    		for (Hand hand : hands) { 
+	    			Vector palmPos =iBox.normalizePoint(hand.palmPosition(),false).minus(midPoint);
+	    			Vector palmVel =hand.palmVelocity();
+
+	    			
+	    			if (palmVel.getY()>150){timeout=0;}
+		    		if (palmVel.getY()<-100 &&timeout<=0) {
 		    			isAbove = false; // you've gone below the drum.
 			    		// radius is how far from the centre the drum was hit.
-			    		double radius = Math.sqrt(Math.pow(pointablePos.getX(), 2.0) + Math.pow(pointablePos.getZ(), 2.0));
-			    		if (radius < 0.5) { // if you hit the actual drum
+			    		double radius = Math.sqrt(Math.pow(palmPos.getX(), 2.0) + Math.pow(palmPos.getZ(), 2.0));
+			    		if (radius <Radius) { // if you hit the actual drum
 			    			// This is currently a would be nice; scale volume according to how hard the drum was hit.
-			    			float volume = - pointable.tipVelocity().getY() / 4500f; 
+			    			float volume = - palmVel.getY() / 1000f; 
 			    			// speed of impact = downward velocity, normalized to a fast drumstick impact speed.
 			    			volume = Math.min(1.0f, volume);
-			    			if ( volume > 0 ) notes[(int) (10*radius)] = true; // start sound from impact.
+			    			if ( volume > 0 ) notes[(int) (10*radius)] = true; 
+			    			timeout=5;// start sound from impact.
 			    		}
-		    		} else if (pointablePos.getY() > 0) {
-		    			isAbove = true; // You've gone above the drum.
-		    		}
+
 	    		}
 	    	}
-    	}	
+    	}
     }
 
 	@Override
@@ -91,33 +101,40 @@ public class Drum extends Listener implements DiscreteInputDisplay{
 
 	@Override
 	public void draw(SpriteBatch batch) {
+		
 		batch.setColor(Color.WHITE);
-		// draw drum background
-		// drum centre at left + half width, top + half height
-		// x: MusicalFingers.width/2f
-		// y: MusicalFingers.height/6f + drumImg.getHeight()*sF / 2f
+
 		
 		batch.draw(drumImg, MusicalFingers.width/2f - drumImg.getWidth()/2f*sF, MusicalFingers.height/6f,drumImg.getWidth()*sF,drumImg.getHeight()*sF);
 		
-		
-		
+		float mid=0;
+		for(Hand hand : hands)	{mid =hand.palmPosition().getX() +mid;}
+		mid =mid/2;
 		// circle size = 7/8 of image height. 
-		if (pointableList != null) {
-			for (Pointable pointable : pointableList) {
-				// Pointable pointable = lowestPointable(pointableList);
-				if (null != iBox && iBox.isValid()) {
-				// pointablePos returns a vector indicating the position of the pointable tip such that
-				// 	(0,0,0) is the centre of the interaction box, range from -0.5 to 0.5 in all dimensions
-				Vector pointablePos = vectorMinus ( iBox.normalizePoint(pointable.tipPosition(),false) , midPoint ) ;
-				// batch.draw (fingerPoint, -half Circle Width + centre of image + proportional offset on drum, Similar for height , Circle Width, Circle Height)
-				batch.draw(fingerPoint,-6f + (MusicalFingers.width/2f) + (pointablePos.getX() * drumImg.getHeight()*sF * 7f / 8f), -6f + (MusicalFingers.height/6f + drumImg.getHeight()*sF / 2f) - (pointablePos.getZ() * drumImg.getHeight()*sF * 7f / 8f) ,12,12);
-				batch.draw(rectangle, MusicalFingers.width/2f - 5*10f*sF, 49 + pointablePos.getY() * 5 * sF,100*sF,sF );
+		for(Hand hand : hands)	{ 
+				Vector palmPos = iBox.normalizePoint(hand.palmPosition(),false).minus(midPoint).times(scaleFactor);
+				batch.draw(fingerPoint,-6f + (MusicalFingers.width/2f) + (palmPos.getX() * drumImg.getHeight()*sF * 7f / 8f), -6f + (MusicalFingers.height/6f + drumImg.getHeight()*sF / 2f) - (palmPos.getZ() * drumImg.getHeight()*sF * 7f / 8f) ,12,12);
+				//batch.draw(fingerPoint,-12f+MusicalFingers.width/2f - (drumImg.getWidth()/2f*sF) +drumImg.getWidth()*sF*palmPos.getX(),-12f+MusicalFingers.height/6f+(6+64*(1f-1f*palmPos.getZ()))*sF,24,24);
+				/*if (hands.count()==1) {
+					batch.setColor(Color.WHITE);
+					batch.draw(rectangle, MusicalFingers.width/2f - 5*10f*sF+sF*25, 49 + palmPos.getY() * 5 * sF,50*sF,sF );
 				}
-			}
-		}
-		// indication of drum level.		
-		batch.draw(rectangle,MusicalFingers.width/2f - 5*10f*sF,49,100*sF,sF);
+				else if (hand.palmPosition().getX()>mid ) {
+					batch.setColor(Color.RED);
+					batch.draw(rectangle, MusicalFingers.width/2f - 5*10f*sF+sF*50, 49 + palmPos.getY() * 5 * sF,50*sF,sF );
+				}
+				else {
+					batch.setColor(Color.BLUE);
+					batch.draw(rectangle, MusicalFingers.width/2f - 5*10f*sF, 49 + palmPos.getY() * 5 * sF,50*sF,sF );
+				} */
 
+		
+		}
+
+		// indication of drum level.	
+		//batch.setColor(Color.WHITE);
+		//batch.draw(rectangle,MusicalFingers.width/2f - 5*10f*sF,49,100*sF,sF);
+		
 		
 	}
 
